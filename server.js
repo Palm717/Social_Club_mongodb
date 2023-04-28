d2274726a67f8133310ffd2aaf9b0956561e662a
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-04-28 13:09:59 -0400

    lunch break

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// require connection from mongoDB
+const connection = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express());
