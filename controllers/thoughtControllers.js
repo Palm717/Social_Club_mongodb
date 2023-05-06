5c9e561dc20aa836516c47ecdf0b1dc446c2a577
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-05-06 10:15:27 -0400

    finishing thought logic and routes

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -23,8 +23,46 @@ const getAllThoughts = async (req, res) => {
   }
 };
 
+const getThoughtById = async (req, res) => {
+  const thoughtId = req.params.id;
+  try {
+    const thought = await Thought.findById(thoughtId).populate("username");
+
+    if (!thought) {
+      res.status(404).json({ error: `No thoughts found with ID ${thoughtId}` });
+    } else {
+      res.status(200).json({ thought: `Thought found` });
+      res.send(thought);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateThoughtById = async (req, res) => {
+  const thoughtId = req.params.id;
+  const thoughtBody = req.body;
+
+  try {
+    const thought = await User.findByIdAndUpdate(thoughtId, thoughtBody, {
+      new: true,
+    });
+    if (!thought) {
+      res.status(404).json({ error: `Cannot find thought by ID ${thoughtId}` });
+    } else {
+      res.status(200).json(thoughtBody);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+cons;
+
 // export thought fucntion
 module.exports = {
   createThought,
   getAllThoughts,
+  getThoughtById,
+  updateThoughtById,
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,8 +2,11 @@ const router = require("express").Router();
 const {
   createThought,
   getAllThoughts,
+  getThoughtById,
+  updateThoughtById,
 } = require("../../controllers/thoughtControllers");
 
 router.route("/").post(createThought).get(getAllThoughts);
+router.route("/:id").get(getThoughtById).put(updateThoughtById);
 
 module.exports = router;
